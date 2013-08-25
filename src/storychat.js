04c00e4f21680a27acb79c36610a305e663e57a7
Kat Marchán <kzm@sykosomatic.org>
2013-08-25 01:03:39 -0700

    Nodejs side of things is now a glorified broadcasting echo server

--- src/storychat.js
+++ src/storychat.js
@@ -3,7 +3,9 @@
 "use strict";
 
 var express = require("express"),
-    http = require("http");
+    http = require("http"),
+    Sockjs = require("sockjs"),
+    _ = require("lodash");
 
 /*
  * App Setup
@@ -18,14 +20,31 @@ app.configure(function(){
 	app.use(express["static"](__dirname + "/../static"));
 });
 
-var server = http.createServer(app);
-var Sockjs = require("sockjs"),
-    sock = Sockjs.createServer();
+var server = http.createServer(app),
+    sock = Sockjs.createServer(),
+    connections = [];
 
 sock.on("connection", function(socket) {
-  socket.write(JSON.stringify({foo: "bar"}));
+  console.log("Received connection from "+socket.remoteAddress+".");
+  connections.push(socket);
+  socket.on("data", function(data) {
+    _.each(connections, _.partialRight(method("write"), data));
+  });
+  socket.on("close", function() {
+    console.log("Client at "+socket.remoteAddress+" disconnected.");
+    connections = _.without(connections, socket);
+  });
 });
 
 sock.installHandlers(server, {prefix: "/ws"});
 
 server.listen(8080);
+
+/*
+ * Utils
+ */
+function method(name) {
+  return function(obj) {
+    return obj[name].apply(obj, [].slice.call(arguments, 1));
+  };
+}
