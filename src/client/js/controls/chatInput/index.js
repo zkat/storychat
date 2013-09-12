0efbf4f421edf1adfa803ef242f6b299059bfbe7
Kat Marchán <kzm@sykosomatic.org>
2013-09-12 00:13:35 -0700

    Getting rid of inputs users shouldn't have access to

--- src/client/js/controls/chatInput/index.js
+++ src/client/js/controls/chatInput/index.js
@@ -28,12 +28,10 @@ let listener = clone(EventListener, {
 });
 
 // NOTE: Yeah, it has to be added in multiple places.
-let inputTypes = ["dialogue", "action", "slug", "heading", "ooc"];
+let inputTypes = ["dialogue", "action", "ooc"];
 let inputTemplates = {
   dialogue: require("./inputs/dialogue.mustache"),
   action: require("./inputs/action.mustache"),
-  slug: require("./inputs/slug.mustache"),
-  heading: require("./inputs/heading.mustache"),
   ooc: require("./inputs/ooc.mustache")
 };
 
