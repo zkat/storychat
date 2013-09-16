f209a438fde4edba72f793a8ede5d4987ba305d6
Kat Marchán <kzm@sykosomatic.org>
2013-09-16 08:08:37 -0700

    Use an additional salt external to the db

--- src/server/user/db.js
+++ src/server/user/db.js
@@ -2,7 +2,9 @@
 /* vim: set ft=javascript ts=2 et sw=2 tw=80; */
 "use strict";
 
-let db = require("../db");
+let db = require("../db"),
+    fs = require("fs"),
+    config = JSON.parse(fs.readFileSync(__dirname+"/../../../config/app.json"));
 
 let bfIterations = 9;
 
@@ -14,7 +16,7 @@ function create(email, displayName, password) {
   return db.query(q, {
     email: email,
     displayName: displayName,
-    password: password,
+    password: password + config.passwordSalt,
     iterations: bfIterations
   });
 }
