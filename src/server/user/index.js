8924ae1a440decd3ba2a5d8fc72de4083d7d1590
Kat Marchán <kzm@sykosomatic.org>
2013-09-16 21:33:25 -0700

    Verifier returns promises now

--- src/server/user/index.js
+++ src/server/user/index.js
@@ -3,16 +3,23 @@
 "use strict";
 
 let db = require("./db"),
-    check = require("validator").check,
+    check = require("../check"),
     sanitize = require("validator").sanitize;
 
 function create(email, displayName, password, passwordVerification) {
   email = sanitize(email).trim();
   displayName = sanitize(displayName).trim();
-  check(email).isEmail();
-  check(displayName).len(4, 30);
-  check(password).len(6, 64).equals(passwordVerification);
-  return db.create(email, displayName, password);
+  let v = check();
+  v.check(email, "Valid email required").isEmail();
+  v.check(displayName, "Display name must be between 4 and 30 chars")
+    .len(4, 30);
+  v.check(password, "Passwords must be between 6 and 64 chars")
+    .len(6, 64);
+  v.check(passwordVerification, "Verification must match password")
+    .equals(password);
+  return v.done().then(function() {
+    return db.create(email, displayName, password);
+  });
 }
 
 function verify(email, password) {
