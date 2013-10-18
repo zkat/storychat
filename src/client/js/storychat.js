00c21ba5eda7683bf37a8952792f68e049fd4526
Kat Marchán <kzm@sykosomatic.org>
2013-10-17 23:36:25 -0700

    Use the new connect() interface in storychat.js

--- src/client/js/storychat.js
+++ src/client/js/storychat.js
@@ -1,12 +1,12 @@
 "use strict";
 
 let $ = require("jquery"),
-    {SocketConn} = require("./lib/socketConn"),
+    {connect} = require("./lib/socketConn"),
     {Router} = require("./router"),
     {clone} = require("./lib/proto");
 
 $(function() {
   let origin = window.location.protocol + "//" + window.location.host;
-  window.socketConn = clone(SocketConn, origin + "/wsauth");
+  window.socketConn = connect(origin + "/wsauth");
   window.router = clone(Router, window.socketConn);
 });
