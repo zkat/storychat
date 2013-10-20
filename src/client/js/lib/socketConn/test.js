4a45d47b1813e784ae96917a18ba148ed898ed91
Kat Marchán <kzm@sykosomatic.org>
2013-10-19 21:40:01 -0700

    New interface tests for socketConn

--- src/client/js/lib/socketConn/test.js
+++ src/client/js/lib/socketConn/test.js
@@ -3,7 +3,9 @@
 
 let assert = require("assert");
 
-let socketConn = require("./index");
+let socketConn = require("./index"),
+    {connect, disconnect, onMessage,
+     listen, unlisten, send, request} = socketConn;
 
 let {addMethod} = require("genfun");
 
@@ -12,69 +14,97 @@ let origin = "http://localhost:8080";
 describe("socketConn", function() {
 
   describe("connect()", function() {
-    it("accepts a websocket url to connect to", function(done) {
-      let conn = socketConn.connect(origin + "/wsauth"),
-          obj = {};
+    it("accepts a websocket url to connect to", function() {
+      let conn = connect(origin + "/wsauth");
       assert.ok(conn);
-      addMethod(socketConn.onOpen, [obj], function() {
-        socketConn.disconnect(conn);
-        done();
-      });
-      socketConn.listen(conn, obj, "test");
+      disconnect(conn);
     });
-  });
-
-  describe("onOpen()", function() {
-    it("is called on observers when a connection opens", function(done) {
-      let conn = socketConn.connect(origin + "/wsauth"),
-          obj = {};
-      addMethod(socketConn.onOpen, [obj], function() {
-        assert.ok(true);
-        socketConn.disconnect(conn);
-        done();
-      });
-      socketConn.listen(conn, obj, "test");
-    });
-  });
-
-  describe("onMessage()", function() {
-    it("is called on observers when a message is received");
-  });
-
-  describe("onClose()", function() {
-    it("is called on observers when a connection closes");
-  });
-
-  describe("listen()", function() {
-    it("registers an observer with a connection");
-  });
-
-  describe("unlisten()", function() {
-    it("unregisters an observer with a connection");
-  });
-
-  describe("send()", function() {
-    it("sends a message to the server");
-  });
-
-  describe("request()", function() {
-    it("returns a promise that resolves to a message's response");
-    it("does not trigger onMessage when the response arrives");
   });
 
   describe("disconnect()", function() {
-    it("accepts a socket connection", function(done) {
-      let conn = socketConn.connect(origin + "/wsauth"),
-          obj = {};
-      addMethod(socketConn.onOpen, [obj], function() {
-        socketConn.disconnect(conn);
+    it("accepts a socket connection", function() {
+      let conn = connect(origin + "/wsauth");
+      disconnect(conn);
+      assert.equal(conn.state(), "closed");
+      assert.throws(function() {
+        send(conn, "test", {content: "test"});
+      }, /connection is closed/);
+    });
+  });
+
+  describe("#state()", function() {
+    it("returns the current state of the connection");
+    it("is an observable value");
+  });
+
+  describe("onMessage()", function() {
+    it("is called when a message is received opens", function(done) {
+      let conn = connect(origin + "/wsauth"),
+          observer = {};
+      addMethod(onMessage, [conn, observer], function(conn1, obs, msg) {
+        assert.equal(obs, observer);
+        assert.equal(conn1, conn);
+        assert.equal(msg.content, "test");
+        disconnect(conn1);
+        done();
       });
-      addMethod(socketConn.onClose, [obj], function() {
+      listen(conn, observer, "test");
+      send(conn, "test", {content: "test"});
+    });
+  });
+
+  describe("listen()", function() {
+    it("registers an observer with a connection", function(done) {
+      let conn = connect(origin + "/wsauth"),
+          observer = {};
+      addMethod(onMessage, [conn, observer], function() {
         assert.ok(true);
         done();
       });
-      socketConn.listen(conn, obj, "test");
+      listen(conn, observer, "test");
+      send(conn, "test", {content: "test"});
     });
+  });
+
+  describe("unlisten()", function() {
+    it("unregisters an observer with a connection", function(done) {
+      let conn = connect(origin + "/wsauth"),
+          observer = {};
+      addMethod(onMessage, [conn, observer], function() {
+        assert.ok(true);
+        done();
+      });
+      listen(conn, observer, "test");
+      unlisten(conn, observer, "test");
+      send(conn, "test", {content: "test"});
+    });
+  });
+
+  describe("send()", function() {
+    it("sends a message to the server", function(done) {
+      let conn = connect(origin + "/wsauth"),
+          observer = {};
+      addMethod(onMessage, [conn, observer], function(conn1, obs, msg) {
+        assert.equal(msg.content, "test");
+        disconnect(conn1);
+        done();
+      });
+      listen(conn, observer, "test");
+      send(conn, "test", {content: "test"});
+    });
+  });
+
+  describe("request()", function() {
+    it("returns a promise that resolves to a response", function(done) {
+      let conn = connect(origin + "/wsauth"),
+          promise = request(conn, "test", {content: "test"});
+      assert.equal(typeof promise.then, "function");
+      promise.then(function(resp) {
+        assert.equal(resp.content, "test");
+        done();
+      }, done);
+    });
+    it("does not trigger onMessage when the response arrives");
   });
 
 });
