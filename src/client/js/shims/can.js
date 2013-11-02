946fd6e497bdb5c73ea4a300062058081db8e41f
Kat Marchán <kzm@sykosomatic.org>
2013-11-01 22:53:20 -0700

    Work around can2.0 compatibility issue with jquery2.0

--- src/client/js/shims/can.js
+++ src/client/js/shims/can.js
@@ -1,6 +1,17 @@
 "use strict";
 
-require("jquery");
+let $ = require("jquery"),
+    oldDomManip = $.fn.domManip;
+
+$.fn.domManip = function(args, table, callback) {
+  return oldDomManip.call(this, args, callback, table);
+};
+
 require("../../../../bower_components/canjs/can.jquery.js");
+
+let canDomManip = $.fn.domManip;
+$.fn.domManip = function(args, callback, whatever) {
+  return canDomManip.call(this, args, whatever, callback);
+};
 
 module.exports = window.can;
