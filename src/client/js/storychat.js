81a0a2fa1f64a903cfb2a750dcba2025dbd3e364
Flarnie Marchan <fnonemaker@gmail.com>
2013-11-22 20:20:54 -0800

    Added css reset

--- src/client/js/storychat.js
+++ src/client/js/storychat.js
@@ -4,6 +4,9 @@ let $ = require("jquery"),
     {Router} = require("./router"),
     {clone} = require("./lib/proto");
 
+let style = require("./lib/ensureStyle");
+
 $(function() {
+  style(require("../css/reset.styl"));
   window.router = clone(Router);
 });
