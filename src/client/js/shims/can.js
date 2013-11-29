db40c19ef4339d899893d5aba2915fe93d2acc7f
Kat Marchán <kzm@sykosomatic.org>
2013-11-29 12:31:23 -0800

    This hack is no longer needed after the latest canjs

--- src/client/js/shims/can.js
+++ src/client/js/shims/can.js
@@ -1,17 +1,5 @@
 "use strict";
 
-let $ = require("jquery"),
-    oldDomManip = $.fn.domManip;
-
-$.fn.domManip = function(args, table, callback) {
-  return oldDomManip.call(this, args, callback, table);
-};
-
 require("../../../../bower_components/canjs/can.jquery.js");
-
-let canDomManip = $.fn.domManip;
-$.fn.domManip = function(args, callback, whatever) {
-  return canDomManip.call(this, args, whatever, callback);
-};
 
 module.exports = window.can;
